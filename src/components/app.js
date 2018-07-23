ad1001ed18ba074001d1f0033a9205311c8a5ae8
sasa-us <sasa_luu@hotmail.com>
2018-07-23 16:23:31 -0700

    nav bar

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,10 @@ import '../assets/css/app.css';
 import { Route } from 'react-router-dom';
 import ChatRoom from './chat_room';
 import Home from './home';
+import Nav from './nav';
 const App = () => (
     <div>
+        <Nav />
         <div className="container"> 
             {/* <h1 className="center">Fire Chat</h1> */}
             <Route exact path="/" component={Home} />
